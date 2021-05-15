c1fa18533d9a99cf61d7d3fd593144199c9a63ed
123 123 <lait@it-ktk.ru>
2021-05-15 10:46:17 +0500

    Another change

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,10 +6,11 @@ namespace ConsoleApp5
 	{
 		static void Main(string[] args)
 		{
-			int length = 10;
+			int length = 20;
 			int[] arr = new int[length];
 			bool isNumberOk = false;
 			int diaposon = 0;
+			int numverToSearch = 0;
 
 			while (!isNumberOk)
 			{
@@ -17,7 +18,11 @@ namespace ConsoleApp5
 				{
 					Console.Write("Enter diaposon from 0 to 'Your value': ");
 					diaposon = Int32.Parse(Console.ReadLine());
+
 					isNumberOk = true;
+
+					Console.Write("Enter number to search for: ");
+					numverToSearch = Int32.Parse(Console.ReadLine());
 				}
 
 				catch
@@ -25,7 +30,11 @@ namespace ConsoleApp5
 					Console.WriteLine("Enter a number: ");
 				}
 			}
-			
+
+			int position = -1;
+
+			bool found = false;		
+
 			Random rnd = new Random();
 
 			for(int i = 0; i < arr.Length; i++)
@@ -36,36 +45,51 @@ namespace ConsoleApp5
 			Console.WriteLine();
 			Console.WriteLine("Origin array");
 
-			for(int i = 0; i < arr.Length; i++)
-			{
-				Console.Write(arr[i] + " ");
-			}
-
-			int temp;
-
-			for(int i = 0; i < arr.Length; i++)
-			{
-				for(int j = 1; j < arr.Length - i; j++)
-				{
-					if(arr[j-1] > arr[j])
-					{
-						temp = arr[j - 1];
-						arr[j - 1] = arr[j];
-						arr[j] = temp;
-					}
-				}
-			}
-
-			Console.WriteLine();
-
-			Console.WriteLine();
-			Console.WriteLine("Sorted  array");
-
 			for (int i = 0; i < arr.Length; i++)
 			{
 				Console.Write(arr[i] + " ");
 			}
 
+			Console.WriteLine();
+
+			
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if(arr[i] == numverToSearch)
+				{
+					found = true;
+					position = i;
+					break;
+				}			
+			}
+			
+
+			if(found == true)
+			{
+				switch (position)
+				{
+					case 1:
+						Console.WriteLine($"The number {numverToSearch} is located at {position}st position");
+						break;
+
+					case 2:
+						Console.WriteLine($"The number {numverToSearch} is located at {position}nd position");
+						break;
+
+					case 3:
+						Console.WriteLine($"The number {numverToSearch} is located at {position}rd position");
+						break;
+
+					default:
+						Console.WriteLine($"The number {numverToSearch} is located at {position}th position");
+						break;
+				}
+			}
+
+			if(found == false)
+			{
+				Console.WriteLine("This number doesn't exist int this array!!");
+			}
 		}
 	}
 }
